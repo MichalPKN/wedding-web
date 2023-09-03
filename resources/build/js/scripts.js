b8c1b02a2739faf04a5a7e102acc10f8ec59112e
MichalPKN <michal.pokorny24@gmail.com>
2023-09-03 11:33:36 +0200

    hero imgs animation

--- resources/build/js/scripts.js
+++ resources/build/js/scripts.js
@@ -233,11 +233,35 @@ if (document.querySelector(".c-hero")) {
         tl.from(".c-hero__leaves", {y:window.innerWidth > 952 ? 50/1920*100 + "vw" : 30, opacity:0, ease: "power2.out", duration:1.3}, "<");
         tl.from(".c-hero__photo1", {y:window.innerWidth > 952 ? 200/1920*100 + "vw" : 100, opacity:0, ease: "power2.out", duration:1.3}, "<");
         tl.from(".c-hero__photo2", {y:window.innerWidth > 952 ? 200/1920*100 + "vw" : 100, opacity:0, ease: "power2.out", duration:1.3}, "<");
+
+        gsap.to(".c-hero__imgWrap1", {
+            y:"-15%", 
+            scrollTrigger: {
+                trigger:".c-hero",
+                scrub:0.4,
+                start: "top top",
+                end: "bottom top",
+                markers: true
+            }
+        });
+
+        gsap.to(".c-hero__imgWrap2", {
+            y:"-30%", 
+            scrollTrigger: {
+                trigger:".c-hero",
+                scrub:0.4,
+                start: "top top",
+                end: "bottom top",
+                markers: true
+            }
+        });
+
         if(window.innerWidth < 953) {
             tl.from(".c-hero__title", {"--y-offset":"-310%", ease: "power2.out", duration:1.3}, "<");
         }else{
             tl.from(".c-hero__title", {"--x-offset":"165%", ease: "power2.out", duration:1.3}, "<");
         }
+        
     })
 }
 if (document.querySelector(".c-location")) {
